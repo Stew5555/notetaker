d97f5a2df109bc71e3520e5d391eb7c0d5ed9865
Stew5555 <104962519+Stew5555@users.noreply.github.com>
2022-08-30 14:23:46 -0700

    routing the apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,15 @@
+// dependencies
+const path = require('path');
 const fs = require("fs");
 
+// calling for uniqid from npm package
+var uniqid = require('uniqid');
+
+// routing files
+module.exports = (app) => {
+    app.get('/api/notes', (req, res) =>{
+        res.sendFile(path.join(__dirname, '../db/db.json'));
+    });
+
+    
+}
