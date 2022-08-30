cadf7d111ac3c732e5baa10c8c0b1a03513f6e57
Stew5555 <104962519+Stew5555@users.noreply.github.com>
2022-08-29 18:06:31 -0700

    setting up port and routes in server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+// app uses express
+
+const app = express();
+
+//creates variable port
+const port = process.env.port || 3001;
+
+//creates route for files in the public folder 
+app.use
+
